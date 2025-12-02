deede1200a406667ed54fff410460ac45619f527
mhmharshani <mhmharshani@gmail.com>
2025-12-03 01:32:36 +0530

    UI completed

---
+++ assests/js/app_customer.js
@@ -0,0 +1,13 @@
+// const inputField = document.getElementById('myInput');
+
+// // Make the input editable when it is clicked/focused
+// inputField.addEventListener('click', function() {
+//     // Remove the readonly attribute
+//     this.removeAttribute('readonly'); 
+// });
+
+// // Make the input non-editable (readonly) when it loses focus (blurs)
+// inputField.addEventListener('blur', function() {
+//     // Add the readonly attribute back
+//     this.setAttribute('readonly', 'readonly');
+// });
